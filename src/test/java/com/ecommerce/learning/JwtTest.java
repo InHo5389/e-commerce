c851e8698d11f1d7377ddd908de5acfba3acf8ca
InHo5389 <80667642+InHo5389@users.noreply.github.com>
2023-05-16 18:29:51 +0900

    Revert "feat: [001-jwt-login] 로그인 성공을 하면 jwt 토큰방식을 하여 요청,응답 기능 추가"

--- src/test/java/com/ecommerce/learning/JwtTest.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.learning;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-@SpringBootTest
-public class JwtTest {
-}
